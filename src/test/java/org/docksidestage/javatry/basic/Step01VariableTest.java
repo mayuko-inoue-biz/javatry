2a2aa0b7c67e4d7e8ddb356075ea90fcbffe59bf
inouemayuko <m.inoue@bizreach.co.jp>
2024-07-01 10:46:33 +0900

    variable_instance_variable_default_String を解いた

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -109,7 +109,8 @@ public class Step01VariableTest extends PlainTestCase {
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_String() {
         String sea = instanceBroadway;
-        log(sea); // your answer? => 
+        log(sea); // your answer? =>  null(理由：参照型なため、初期値は何も指してないnullが入りそう)
+        //答え：null
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
