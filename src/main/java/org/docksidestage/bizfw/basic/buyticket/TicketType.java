9994d9b60993006c8c678dfbeb79a4e130775879
inouemayuko <m.inoue@bizreach.co.jp>
2024-08-30 08:45:25 +0900

    TicketType の enum にコメントを追加した

--- src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketType.java
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/TicketType.java
@@ -4,19 +4,31 @@ package org.docksidestage.bizfw.basic.buyticket;
  * @author mayukorin
  */
 public enum TicketType {
+    // ===================================================================================
+    //                                                                           Identifier
+    //                                                                           =========
     ONE_DAY_PASSPORT(1, 7400, 10),
     TWO_DAY_PASSPORT(2, 13200, 10); // MAX_QUANTITY を入れられなくなってしまった...
 
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
     private final int initialAvailableDays;
     private final int price;
     private final int initialQuantity;
 
+    // ===================================================================================
+    //                                                                         Constructor
+    //                                                                         ===========
     private TicketType(int initialAvailableDays, int price, int initialQuantity) {
         this.initialAvailableDays = initialAvailableDays;
         this.price = price;
         this.initialQuantity = initialQuantity;
     }
 
+    // ===================================================================================
+    //                                                                            Accessor
+    //                                                                            ========
     public int getInitialAvailableDays() {
         return initialAvailableDays;
     }
