7c3a87ab9b3d41ee01db8192bfa28e509031002d
inouemayuko <m.inoue@bizreach.co.jp>
2024-10-16 13:51:17 +0900

    step05の修行++とbarking関連のクラスをサブパッケージにまとめる問題以外は done にした

--- src/test/java/org/docksidestage/javatry/basic/st6/dbms/St6QL.java
+++ src/test/java/org/docksidestage/javatry/basic/st6/dbms/St6QL.java
@@ -7,7 +7,7 @@ package org.docksidestage.javatry.basic.st6.dbms;
 // SQL よりも大きな枠組みに「クエリ言語」という概念がある
 // 「クエリ言語」自体にPagingQueryの仕様があると考えて良いかも
 // なので抽象クラスの名前 SQL → QL にしてみる？
-// TODO mayukorin [ふぉろー] 考えてくださりありがとうございます。考えること自体がトレーニングなのでOKです by jflute (2024/10/14)
+// TODO done mayukorin [ふぉろー] 考えてくださりありがとうございます。考えること自体がトレーニングなのでOKです by jflute (2024/10/14)
 // ある程度MySQLとPostgreSQLを知らないとわからないことではあって、MySQLとかPostgreSQLでググってそれが何であるのか？
 // 「MySQL PostgreSQL 何？」でググってみると、GoogleのAIが「どちらもリレーショナルデータベース管理システム（RDBMS）として」って言います。
 // そして「RDBMS 種類」でググってみると、「MySQL、PostgreSQL、MariaDB、Microsoft SQL Server、Oracle Database、 IBM DB2」と言います。
@@ -15,6 +15,8 @@ package org.docksidestage.javatry.basic.st6.dbms;
 // その概念を紐解くとMySQLとPostgreSQLは、SQLではなくDBなんですね。
 // DBという言葉が抽象的なので、製品の違いを意識するときはよく「DBMS」と呼ばれます。
 // 実は、このクラスのpackageはdbmsパッケージだったりします。
+// なるほど...!! 確かに、MySQL PostgreSQLは、SQL というよりは DBMS の概念ですよね！
+// パッケージ名もヒントになっていたとは...教えていただきありがとうございます！ by m.inoue (2024/10/16)
 /**
  * The object for QL (Query Language).
  * @author mayukorin
